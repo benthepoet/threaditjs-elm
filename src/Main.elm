9097580ce25b837fc37a84ba43a32f954c5f58fd
Ben Hanna <benpaulhanna@gmail.com>
2017-07-22 03:19:26 +0000

    Added decoders and fetch function for threads.

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,52 @@
-import Html exposing (Html, div, text)
+import Html exposing (Html, button, div, li, text, ul)
+import Html.Attributes exposing (type_)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 import Navigation
 
 
 type alias Model =
-    { location: Navigation.Location
+    { location : Navigation.Location
+    , threads : List Thread
     }
 
 type Msg 
     = LocationChange Navigation.Location
+    | ProcessThreads (Result Http.Error (List Thread))
+    | GetThreads
     
+    
+type alias Thread =
+    { id : String
+    , commentCount : Int
+    , text : String
+    }
+    
+
+threadListDecoder =
+    Decode.at ["data"]
+        <| Decode.list threadDecoder
+
+
+threadDecoder =
+    Decode.map3 Thread
+        (Decode.field "id" Decode.string)
+        (Decode.field "comment_count" Decode.int)
+        (Decode.field "text" Decode.string)
+
+
+getThreads =
+    let
+        url = "https://api.threaditjs.com/threads"
+    in
+        Http.send ProcessThreads
+            <| Http.get url threadListDecoder
+
+
+renderThreads threads =
+    List.map (\l -> li [] [ text l.text ]) threads
+
 
 main = 
     Navigation.program LocationChange
@@ -21,7 +59,7 @@ main =
     
 init : Navigation.Location -> (Model, Cmd Msg)
 init location =
-    ( Model location
+    ( Model location []
     , Cmd.none
     )
     
@@ -34,7 +72,21 @@ update msg model =
             , Cmd.none
             )
             
+        GetThreads ->
+            (model, getThreads)
+            
+        ProcessThreads (Ok threads) ->
+            ({ model | threads = threads}
+            , Cmd.none
+            )
+            
+        ProcessThreads (Err _) ->
+            (model, Cmd.none)
+            
             
 view : Model -> Html Msg
 view model =
-    div [] [ text model.location.hash ]
+    div [] 
+        [ button [ type_ "button", onClick GetThreads ] [ text "Get Threads" ]
+        , ul [] (renderThreads model.threads)
+        ]
